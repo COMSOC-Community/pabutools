88d06281db745c851c85a17b78a253c5d14ea8e5
Simon-Rey <reysimon@orange.fr>
2025-06-16 17:13:30 +0200

    Docs updated

--- docs-source/make.bat
+++ docs-source/make.bat
@@ -28,11 +28,17 @@ if "%1" == "" goto help
 
 if "%1" == "github" goto github
 
+if "%1" == "githubclean" goto githubclean
+
 %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :github
 %SPHINXBUILD% -b html %SOURCEDIR% %BUILDDIRGITHUB% %SPHINXOPTS% %O%
+goto end
+
+:githubclean
+%SPHINXBUILD% -M clean %SOURCEDIR% %BUILDDIRGITHUB% %SPHINXOPTS% %O%
 goto end
 
 :help
