d0fe8501839b59b9ca2f86f9fc276661ffc74cdf
Wiktor Grzankowski <w.grzankowski@gmail.com>
2025-08-18 22:30:07 +0200

    update test class

--- tests/test_priceability.py
+++ tests/test_priceability.py
@@ -11,6 +11,13 @@ from pabutools.election import Project, Instance, ApprovalProfile, ApprovalBallo
 from pabutools.analysis.priceability import priceable, validate_price_system
 
 
+def approval_profile_to_cardinal_profile(profile: ApprovalProfile) -> CardinalProfile:
+    def approval_ballot_to_cardinal_ballot(ballot: ApprovalBallot) -> CardinalBallot:
+        return CardinalBallot({c: 1 for c in ballot})
+
+    voters = [approval_ballot_to_cardinal_ballot(ballot) for ballot in profile]
+    return CardinalProfile(init=voters)
+
 
 class TestPriceability(TestCase):
     def test_priceable_approval(self):
@@ -45,10 +52,12 @@ class TestPriceability(TestCase):
         self.assertTrue(priceable(instance, profile, allocation).validate())
 
         res = priceable(instance, profile)
-        self.assertTrue(priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
+        self.assertTrue(
+            priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
         self.assertTrue(priceable(instance, profile, res.allocation).validate())
 
-        self.assertTrue(validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
+        self.assertTrue(
+            validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
 
     def test_priceable_approval_2(self):
         # Example from https://arxiv.org/pdf/1911.11747.pdf page 15 (k = 5)
@@ -88,10 +97,12 @@ class TestPriceability(TestCase):
         self.assertTrue(priceable(instance, profile, allocation).validate())
 
         res = priceable(instance, profile)
-        self.assertTrue(priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
+        self.assertTrue(
+            priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
         self.assertTrue(priceable(instance, profile, res.allocation).validate())
 
-        self.assertTrue(validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
+        self.assertTrue(
+            validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
 
     def test_priceable_approval_3(self):
         # Example from http://www.cs.utoronto.ca/~nisarg/papers/priceability.pdf page 13
@@ -137,10 +148,12 @@ class TestPriceability(TestCase):
         self.assertFalse(priceable(instance, profile, allocation).validate())
 
         res = priceable(instance, profile)
-        self.assertTrue(priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
+        self.assertTrue(
+            priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions).validate())
         self.assertTrue(priceable(instance, profile, res.allocation).validate())
 
-        self.assertTrue(validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
+        self.assertTrue(
+            validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions))
 
     def test_priceable_approval_4(self):
         # Example from https://equalshares.net/explanation#example
@@ -181,7 +194,54 @@ class TestPriceability(TestCase):
         # self.assertTrue(priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions, stable=True).validate())
         self.assertTrue(priceable(instance, profile, res.allocation, stable=True).validate())
 
-        self.assertTrue(validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions, stable=True))
+        self.assertTrue(
+            validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions,
+                                  stable=True))
+
+    def test_stable_priceable_cardinal_reduces_to_approval_like_test_4(self):
+        # If cardinal profile contains only binary utilities, the implementation should give the same exact solutions.
+        # The election example is the same as in test_priceable_approval_4
+        p = [
+            Project("bike path", cost=700),
+            Project("outdoor gym", cost=400),
+            Project("new park", cost=250),
+            Project("new playground", cost=200),
+            Project("library for kids", cost=100),
+        ]
+        instance = Instance(p, budget_limit=1100)
+
+        v1 = ApprovalBallot({p[0], p[1]})
+        v2 = ApprovalBallot({p[0], p[1], p[2]})
+        v3 = ApprovalBallot({p[0], p[1]})
+        v4 = ApprovalBallot({p[0], p[1], p[2]})
+        v5 = ApprovalBallot({p[0], p[1], p[2]})
+        v6 = ApprovalBallot({p[0], p[1]})
+        v7 = ApprovalBallot({p[2], p[3], p[4]})
+        v8 = ApprovalBallot({p[3]})
+        v9 = ApprovalBallot({p[3], p[4]})
+        v10 = ApprovalBallot({p[2], p[3], p[4]})
+        v11 = ApprovalBallot({p[0]})
+        approval_profile = ApprovalProfile(init=[v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11])
+        profile = approval_profile_to_cardinal_profile(approval_profile)
+
+        allocation = [p[0], p[1]]
+        self.assertFalse(priceable(instance, profile, allocation, stable=True).validate())
+
+        allocation = [p[0], p[2], p[4]]
+        self.assertFalse(priceable(instance, profile, allocation, stable=True).validate())
+
+        allocation = p[1:]
+        self.assertTrue(priceable(instance, profile, allocation, stable=True).validate())
+
+        res = priceable(instance, profile, stable=True)
+        # This is also not true due to pulp rounding error
+        # self.assertTrue(priceable(instance, profile, res.allocation, res.voter_budget, res.payment_functions,
+        #                           stable=True).validate())
+        self.assertTrue(priceable(instance, profile, res.allocation, stable=True).validate())
+
+        self.assertTrue(
+            validate_price_system(instance, profile, res.allocation, res.voter_budget, res.payment_functions,
+                                  stable=True))
 
     def test_stable_priceable_additive(self):
         # Example from Master's Thesis "Stable Priceability for Additive Utilities" section 2.4
